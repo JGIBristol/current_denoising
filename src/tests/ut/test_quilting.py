c0bd8aa803debd17e572b9d3456d4e814f79929c
Richard Lane <richard.lane@bristol.ac.uk>
2025-07-21 13:28:06 +0100

    test for corner traversal

--- src/tests/ut/test_quilting.py
+++ src/tests/ut/test_quilting.py
@@ -288,8 +288,8 @@ def test_cost_to_graph(simple_candidate_patch):
             ((0, 2), 2),
         ],
         (0, 2): [
-            ((0, 1), 1),
             ("END", 0),
+            ((0, 1), 1),
         ],
     }
 
@@ -298,8 +298,43 @@ def test_cost_to_graph(simple_candidate_patch):
     assert graph == expected_graph
 
 
-def test_cost_to_graph_diagonal(unfilled_image):
+def test_cost_to_graph_diagonal(simple_candidate_patch):
     """
     Check we can correctly build a graph for a cost matrix where we will traverse from
     the left to the top edge
     """
+    # Add some infinities to make the graph viable
+    simple_candidate_patch[0, 0] = np.inf
+    simple_candidate_patch[1, 2] = np.inf
+    simple_candidate_patch[2, 1] = np.inf
+    simple_candidate_patch[2, 2] = np.inf
+
+    expected_graph = {
+        "START": [
+            ((1, 0), 3),
+            ((2, 0), 6),
+        ],
+        (0, 1): [
+            ("END", 0),
+            ((0, 2), 2),
+        ],
+        (0, 2): [
+            ("END", 0),
+            ((0, 1), 1),
+        ],
+        (1, 0): [
+            ((1, 1), 4),
+        ],
+        (1, 1): [
+            ((0, 1), 1),
+        ],
+        (2, 0): [
+            ((1, 0), 3),
+        ],
+    }
+
+    graph = quilting.cost_to_graph(
+        simple_candidate_patch, start="left", end="top"
+    )
+
+    assert graph == expected_graph
