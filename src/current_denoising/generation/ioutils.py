39e260741c8b60dddaae30087e5da44dc040d59d
Richard Lane <richard.lane@bristol.ac.uk>
2025-08-28 12:11:16 +0100

    Extract tiles from an image at random
    Has functionality to constrain the tiles based on their RMS or latitude

--- src/current_denoising/generation/ioutils.py
+++ src/current_denoising/generation/ioutils.py
@@ -35,3 +35,155 @@ def read_currents(path: pathlib.Path) -> np.ndarray:
             raise IOError("Close marker does not match the opener.")
 
     return data.reshape(shape)
+
+
+def _included_indices(
+    n_rows: int, tile_size: int, max_latitude: float
+) -> tuple[int, int]:
+    """
+    Find the range of y-indices to select from, given the size of the input image
+    and the maximum latitude.
+
+    Assumes the input image is centred on the equator and ranges from -90 to 90 degrees latitude.
+    """
+    if max_latitude <= 0:
+        raise IOError("Maximum latitude must be > 0")
+
+    # Given the number of rows in the image, find which latitude each row corresponds to
+    latitudes = np.linspace(90, -90, n_rows, endpoint=True)
+
+    # Check if we have enough allowed latitudes
+    allowed_latitudes = np.sum((latitudes < max_latitude) & (latitudes > -max_latitude))
+    if allowed_latitudes < tile_size:
+        raise IOError(
+            f"Not enough allowed latitudes ({allowed_latitudes}) to fit a tile of size {tile_size}"
+        )
+
+    # Find the first latitude that is <= to the provided maximum
+    for min_row in range(n_rows):
+        if max_latitude >= latitudes[min_row]:
+            break
+    else:
+        raise RuntimeError("No rows found below the provided maximum latitude")
+
+    # Find the last latitude that is >= - the provided minimum
+    for max_row in range(min_row + 1, n_rows - tile_size + 2):
+        index = max_row + tile_size - 1
+        # If we have fallen off the bottom of the image, take the last row
+        if index == n_rows:
+            break
+
+        # If the bottom of the tile lies on exactly the threshold, take this row
+        if latitudes[index] == -max_latitude:
+            break
+
+        # If the bottom of the tile is less than the threshold, take the previous row
+        if latitudes[index] < -max_latitude:
+            max_row -= 1
+            break
+    # Don't raise here if we don't find a row above the max latitude - that's fine, we'll just fall
+    # through and take the last row
+
+    return min_row, max_row
+
+
+def _tile_index(
+    rng, *, input_size: tuple[int, int], max_latitude: float, tile_size: int
+) -> tuple[int, int]:
+    """
+    Generate random (y, x) indices for the top-left corner of a tile within an image
+
+    :param input_img_size: The size of the input image
+    :param tile_size: The size of each tile.
+    :param max_latitude: The maximum latitude for the tiles;
+                         will exclude tiles which extend above/below this latitude N/S.
+
+    :returns: A tuple of (y, x) indices
+    """
+    height_range = _included_indices(input_size[0], tile_size, max_latitude)
+    width_range = (0, input_size[1] - tile_size + 1)
+
+    y_index = int(rng.integers(*height_range))
+    x_index = int(rng.integers(*width_range))
+
+    return y_index, x_index
+
+
+def _tile(input_img: np.ndarray, start: tuple[int, int], size: int) -> np.ndarray:
+    """
+    Extract a tile at the provided location + size from a 2d array
+    """
+    return input_img[start[0] : start[0] + size, start[1] : start[1] + size]
+
+
+def _tile_rms(tile: np.ndarray) -> float:
+    """
+    Calculate the RMS of a tile
+
+    :param tile: the input tile
+    :returns: the RMS of the tile
+    """
+    return np.sqrt(np.mean(tile**2))
+
+
+def extract_tiles(
+    rng: np.random.Generator,
+    input_img: np.ndarray,
+    *,
+    num_tiles: int,
+    max_rms: float,
+    max_latitude: float = 64.0,
+    tile_size: int = 32,
+) -> np.ndarray:
+    """
+    Randomly extract tiles from an input image.
+
+    Tiles are selected such that no part of any tile exceeds the provided maximum latitude;
+    this assumes the input image is centred on the equator and ranges from -90 to 90 degrees latitude.
+
+    :param rng: a seeded numpy random number generator
+    :param input_img: The input image from which to extract tiles.
+    :param tile_size: The size of each tile.
+    :param num_tiles: The number of tiles to extract.
+    :param max_rms: Maximum allowed RMS value in a tile
+    :param max_latitude: The maximum latitude for the tiles;
+                         will exclude tiles which extend above/below this latitude N/S.
+
+    :returns: A numpy array containing the extracted tiles.
+    :raises IOError: if the input image is not 2d
+    :raises IOError: if the input image is smaller than the tile size
+    """
+    if input_img.ndim != 2:
+        raise IOError(f"Input image must be 2d; got shape {input_img.shape}")
+    if input_img.shape[0] < tile_size or input_img.shape[1] < tile_size:
+        raise IOError(
+            f"Tile size must be smaller than image size; got {input_img.shape} but {tile_size=}"
+        )
+
+    # Choose the range of indices to pick from
+    height_range = slice(0, input_img.shape[0] - tile_size + 1)
+    width_range = slice(0, input_img.shape[1] - tile_size + 1)
+
+    tiles = np.empty((num_tiles, tile_size, tile_size), dtype=input_img.dtype)
+    indices_found = 0
+    while indices_found < num_tiles:
+        y, x = _tile_index(
+            rng,
+            input_size=input_img.shape,
+            max_latitude=max_latitude,
+            tile_size=tile_size,
+        )
+
+        tile = _tile(input_img, (y, x), tile_size)
+
+        if tile.shape != (tile_size, tile_size):
+            raise IOError(
+                f"Extracted tile has wrong shape {tile.shape}, expected {(tile_size, tile_size)}"
+            )
+
+        # Check the RMS of the tile
+        if _tile_rms(tile) < max_rms:
+            tiles[indices_found] = tile
+            indices_found += 1
+
+    return tiles
---
+++ src/tests/ut/test_io.py
@@ -0,0 +1,212 @@
+"""
+Tests for the generator IO operations
+"""
+
+import pytest
+
+import numpy as np
+
+from current_denoising.generation import ioutils
+
+
+@pytest.fixture
+def rng() -> np.random.Generator:
+    """A random number generator"""
+    return np.random.default_rng(seed=0)
+
+
+@pytest.fixture
+def image() -> np.ndarray:
+    """An example image"""
+    return np.arange(32).reshape((4, 8))
+
+
+@pytest.fixture
+def tall_image() -> np.ndarray:
+    """
+    A tall example image - 19 rows, 5 columns
+
+    Useful for testing things like the latitude exclusion functions
+    """
+    return np.arange(95).reshape((19, 5))
+
+
+def test_extract_tiles_invalid_image_dim():
+    """
+    Check we get the right error if we pass a 1D or 3D image
+    """
+    num_tiles = 1
+    tile_size = 8
+    with pytest.raises(ioutils.IOError):
+        ioutils.extract_tiles(
+            None, np.empty((32, 32, 3)), num_tiles=num_tiles, max_rms=np.inf
+        )
+
+    with pytest.raises(ioutils.IOError, match="Input image must be 2d"):
+        ioutils.extract_tiles(None, np.empty(32), num_tiles=num_tiles, max_rms=np.inf)
+
+
+def test_extract_tiles_large_tiles():
+    """
+    Check we get the right error if we pass a large tile size
+    """
+    with pytest.raises(ioutils.IOError):
+        ioutils.extract_tiles(
+            None, np.empty((128, 32)), num_tiles=1, max_rms=np.inf, tile_size=64
+        )
+
+    with pytest.raises(ioutils.IOError):
+        ioutils.extract_tiles(
+            None, np.empty((32, 128)), num_tiles=1, max_rms=np.inf, tile_size=64
+        )
+
+
+def test_extract_indices(rng):
+    """
+    Check we get all of, and only, the expected indices
+    """
+    expected_indices = {(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)}
+
+    # Generate enough indices that we expect to always get all of them
+    actual_indices = {
+        ioutils._tile_index(rng, input_size=(3, 4), max_latitude=np.inf, tile_size=2)
+        for _ in range(20)
+    }
+
+    assert actual_indices == expected_indices
+
+
+def test_notimplemented_maxlatitude(rng):
+    """
+    Check that we've still got our NotImplemented check for maximum latitude
+    """
+    with pytest.raises(NotImplementedError):
+        ioutils._tile_index(
+            rng,
+            input_size=(32, 32),
+            max_latitude=45.0,
+            tile_size=8,
+        )
+
+
+def test_tile(image):
+    """
+    Test extracting a tile from an array
+    """
+    expected_tile = np.array([[10, 11], [18, 19]])
+
+    assert np.array_equal(ioutils._tile(image, (1, 2), 2), expected_tile)
+
+
+def test_tile_rms():
+    """
+    Check we calculate RMS properly
+    """
+    tile = np.array([[10, 11], [18, 19]])
+    expected_rms = np.sqrt((10**2 + 11**2 + 18**2 + 19**2) / 4)
+
+    assert ioutils._tile_rms(tile) == pytest.approx(expected_rms)
+
+
+class MockRNG:
+    """
+    Used to mock an RNG - returns a specified sequence of integers
+    """
+
+    def __init__(self, h: int, w: int, tile: int):
+        # These are the indices we'll return
+        self.indices = [(0, 0), (1, 1), (2, 3), (2, 5)]
+        self.return_y = True
+        self.index = 0
+
+    def integers(self, *args) -> int:
+        if self.return_y:
+            self.return_y = False
+            return self.indices[self.index][0]
+        else:
+            self.return_y = True
+            self.index += 1
+            return self.indices[self.index - 1][1]
+        raise
+
+
+def test_extract_tiles(image):
+    """
+    Mocking the RNG to return a predefined sequence, see
+    if we get the right tiles out
+    """
+    num_tiles = 4
+    tile_size = 2
+
+    # Mock the RNG by using this class which just returns some predefined
+    # co-ordinates in order
+    rng = MockRNG(*image.shape, tile_size)
+
+    tiles = ioutils.extract_tiles(
+        rng,
+        image,
+        num_tiles=num_tiles,
+        max_rms=np.inf,
+        tile_size=tile_size,
+        max_latitude=np.inf,
+    )
+
+    assert len(tiles) == num_tiles
+
+    expected = [
+        image[0:2, 0:2],
+        image[1:3, 1:3],
+        image[2:4, 3:5],
+        image[2:4, 5:7],
+    ]
+
+    np.testing.assert_array_equal(tiles[0], expected[0])
+    np.testing.assert_array_equal(tiles[1], expected[1])
+    np.testing.assert_array_equal(tiles[2], expected[2])
+    np.testing.assert_array_equal(tiles[3], expected[3])
+
+
+def test_bad_max_latitude():
+    """
+    Check we get the right error if we pass a nonsensical value (i.e. <0) as the max latitude
+    """
+    ioutils._included_indices(32, 2, 45)
+    with pytest.raises(ioutils.IOError):
+        ioutils._included_indices(32, 8, 0)
+    with pytest.raises(ioutils.IOError):
+        ioutils._included_indices(32, 8, -1)
+
+
+def test_exclude_latitude_indices(tall_image):
+    """
+    Check that when we exclude some latitudes, we get the right range of possible indices
+    for the random choice.
+    """
+    assert ioutils._included_indices(tall_image.shape[0], 2, 64.0) == (3, 14)
+
+    # Check we get the right thing when the maximum latitude lies right on a row
+    assert ioutils._included_indices(tall_image.shape[0], 2, 50.0) == (4, 13)
+
+    # Check we get it right when the max latitude is exactly at the start
+    assert ioutils._included_indices(tall_image.shape[0], 2, 90.0) == (0, 17)
+
+    # Check we get it right when the max latitude is exactly at the end
+    # Turns out this is the same as the above
+    assert ioutils._included_indices(tall_image.shape[0], 2, 90.0) == (0, 17)
+
+
+def test_exclude_latitude_include_all(tall_image):
+    """
+    Check we get the entire range if our latitude is >90, or if the size of the tile pushes it >90
+    """
+    assert ioutils._included_indices(tall_image.shape[0], 2, 90.0) == (0, 17)
+    assert ioutils._included_indices(tall_image.shape[0], 3, 100) == (0, 17)
+
+
+def test_exclude_latitude_too_small(tall_image):
+    """
+    Check the right error is raised if we exclude too many latitudes, such that the provided tile size
+    can't be extracted
+    """
+    with pytest.raises(ioutils.IOError):
+        ioutils._included_indices(tall_image.shape[0], 4, 15.0)
