64df0282581e9140cbbf4a2abf8c9e8b24940f12
Richard Lane <richard.lane@bristol.ac.uk>
2025-09-04 12:11:25 +0100

    function for plotting + setting extent automatically based on grid size

--- src/current_denoising/plotting/maps.py
+++ src/current_denoising/plotting/maps.py
@@ -3,6 +3,7 @@ Utilities for plotting maps of things
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def lat_long_grid(img_shape: tuple[int, int]) -> tuple[np.ndarray, np.ndarray]:
@@ -28,3 +29,44 @@ def lat_long_grid(img_shape: tuple[int, int]) -> tuple[np.ndarray, np.ndarray]:
     ), np.linspace(
         -180 + long_point_size / 2, 180 - long_point_size / 2, width, endpoint=True
     )
+
+
+def imshow(
+    current_grid: np.ndarray, axis: plt.Axes = None, cmap: str = "turbo"
+) -> plt.Figure:
+    """
+    Plot a gridded field of current velocities.
+
+    Adds a colorbar, unless we're plotting on an existing axis (in which case the user
+    has control of this sort of thing).
+
+    :param current_grid: gridded field showing current strength; land should be indicated
+                         with np.nan.
+    :param axis: axis to plot on; if None, a new figure and axis will be created.
+    :param cmap: colormap to use. Defaults to the same one used by Laura in her paper,
+                 doi:10.1017/eds.2023.41
+
+    :returns: the figure containing the plot
+    """
+    new_axis = axis is None
+    if new_axis:
+        fig, axis = plt.subplots(figsize=(10, 5))
+
+    lat, long = lat_long_grid(current_grid.shape)
+    extent = [long[0], long[-1], lat[0], lat[-1]]
+    imshow_kw = {
+        "origin": "upper",
+        "vmin": 0,
+        "vmax": 1.4,
+        "cmap": cmap,
+        "extent": extent,
+    }
+    im = axis.imshow(current_grid, **imshow_kw)
+
+    im.set_extent(extent)
+
+    if new_axis:
+        fig.colorbar(im)
+        fig.tight_layout()
+
+    return axis.get_figure()
