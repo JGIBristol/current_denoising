188e6fcf221f5c98bb7afeb0622583d64c70967a
Richard Lane <richard.lane@bristol.ac.uk>
2025-09-05 17:11:51 +0100

    Horrible implementation for getting a tile at lat/long
    Think the lat/long conventions are still backwards, which is causing
    issues, but it all seems to work....

--- src/tests/ut/test_plotting.py
+++ src/tests/ut/test_plotting.py
@@ -2,6 +2,7 @@
 Tests for plotting utilities
 """
 
+import pytest
 import numpy as np
 
 from current_denoising.plotting import maps
@@ -31,3 +32,105 @@ def test_twelfth_degree_latlong():
 
     b = 180 - 1 / 24
     np.testing.assert_array_equal(long, np.linspace(-b, b, 4320))
+
+
+@pytest.fixture
+def sample_grid() -> np.ndarray:
+    """
+    A sample grid for testing
+    """
+    grid = np.arange(648, dtype=float).reshape((18, 36))
+    np.fill_diagonal(grid, np.nan)
+    return grid
+
+
+def test_get_tile(sample_grid: np.ndarray):
+    """
+    Check we can get the right tile from a grid.
+    """
+    tile = maps.get_tile(sample_grid, (75, -165), 30)
+
+    expected = np.array(
+        [
+            [np.nan, 38, 39],
+            [73, np.nan, 75],
+            [109, 110, np.nan],
+        ]
+    )
+    np.testing.assert_array_equal(tile, expected)
+
+
+def test_get_tile_out_of_bounds(sample_grid: np.ndarray):
+    """
+    Check we get an error if we try to get a tile that doesn't fit in the grid.
+    """
+    size = 30
+    with pytest.raises(maps.LatLongError):
+        maps.get_tile(sample_grid, (100, 0), size)
+    with pytest.raises(maps.LatLongError):
+        maps.get_tile(sample_grid, (-100, 0), size)
+    with pytest.raises(maps.LatLongError):
+        maps.get_tile(sample_grid, (200, 0), size)
+    with pytest.raises(maps.LatLongError):
+        maps.get_tile(sample_grid, (-200, 0), size)
+
+    maps.get_tile(sample_grid, (0, 90), size)
+    maps.get_tile(sample_grid, (0, -90), size)
+    with pytest.raises(maps.LatLongError):
+        maps.get_tile(sample_grid, (0, 190), size)
+    with pytest.raises(maps.LatLongError):
+        maps.get_tile(sample_grid, (0, -190), size)
+
+
+def test_get_tile_equal(sample_grid: np.ndarray):
+    """
+    Check we get the right grid point if we're equidistant between two.
+    """
+    tile = maps.get_tile(sample_grid, (70, -160), 30)
+
+    expected = np.array(
+        [
+            [np.nan, 38, 39],
+            [73, np.nan, 75],
+            [109, 110, np.nan],
+        ]
+    )
+    np.testing.assert_array_equal(tile, expected)
+
+
+def test_get_tile_edge(sample_grid: np.ndarray):
+    """
+    Check we can get a tile that starts at the edge of the grid.
+    """
+    tile = maps.get_tile(sample_grid, (90, -180), 30)
+    expected = np.array(
+        [
+            [np.nan, 1, 2],
+            [36, np.nan, 38],
+            [72, 73, np.nan],
+        ]
+    )
+    np.testing.assert_array_equal(tile, expected)
+
+    tile = maps.get_tile(sample_grid, (75, -180), 30)
+    expected = np.array(
+        [
+            [36, np.nan, 38],
+            [72, 73, np.nan],
+            [108, 109, 110],
+        ]
+    )
+    np.testing.assert_array_equal(tile, expected)
+
+    tile = maps.get_tile(sample_grid, (90, -165), 30)
+    expected = np.array([[1, 2, 3], [np.nan, 38, 39], [73, np.nan, 75]])
+    np.testing.assert_array_equal(tile, expected)
+
+
+def test_not_whole_grid_points(sample_grid: np.ndarray):
+    """
+    Check the right error gets raised if the requested tile size isn't a whole number of grid points.
+    """
+    maps.get_tile(sample_grid, (90, -180), 10)
+    with pytest.raises(maps.LatLongError):
+        maps.get_tile(sample_grid, (90, -180), 15)
