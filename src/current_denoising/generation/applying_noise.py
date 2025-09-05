b15463c73af593aafa7a64d34ea229e7f06d193b
Richard Lane <richard.lane@bristol.ac.uk>
2025-09-05 12:59:03 +0100

    Noise addition: tests + functionality

--- src/current_denoising/generation/applying_noise.py
+++ src/current_denoising/generation/applying_noise.py
@@ -7,22 +7,136 @@ import numpy as np
 from scipy.ndimage import gaussian_filter
 
 
+class NoiseApplicationError(Exception):
+    """General exception for noise application errors."""
+
+
+class RescalingError(NoiseApplicationError):
+    """Error with rescaling the noise strength"""
+
+
+class ReweightingError(NoiseApplicationError):
+    """Error with reweighting synthetic noisy tile to better match reality"""
+
+
 def noise_strength_map(current_grid: np.ndarray, *, filter_size: int) -> np.ndarray:
     """
     Get the expected noise strength map from a gridded field of current
     velocities.
 
+    Sets land values to 0.0, since we don't really care what happens here (there's no signal),
+    but we likely want to avoid NaNs in the output (this might break future calculations).
+
     :param current_grid: gridded field showing current strength; land should be indicated
                          with np.nan.
     :param filter_size: size of Gaussian filter to apply, in grid points
     :returns: a grid of the same shape as current_grid, showing the expected noise strength
-              at that grid point. Land values are set to np.nan.
+              at that grid point. Land values are set to 0.0.
     """
     land_mask = np.isnan(current_grid)
 
     current_grid = np.where(land_mask, 0.0, current_grid)
 
     filtered = gaussian_filter(current_grid, sigma=filter_size)
-    filtered[land_mask] = np.nan
+    filtered[land_mask] = 0.0
 
     return filtered
+
+
+def _weighted_add(
+    clean_target: np.ndarray,
+    noise_quilt: np.ndarray,
+    strength_map: np.ndarray,
+    *,
+    weighting_factor: float,
+) -> np.ndarray:
+    """
+    Add noise to a target.
+
+    :param clean_target: the clean target currents
+    :param noise_quilt: the noise quilt to add
+    :param strength_map: the noise strength map to use for weighting.
+                         Might come from noise_strength_map().
+    :param weighting_factor: a weighting factor applied to the noise before addition.
+                             Probably randomly generated.
+
+    :returns: the noisy target data
+    """
+    if np.isnan(strength_map).any():
+        raise RescalingError("strength_map contains NaNs - these should be set to 0...")
+
+    return clean_target + (noise_quilt * strength_map * weighting_factor)
+
+
+def reweight_noisy_tile(
+    noisy_tile: np.ndarray, clean_tile_maximum: float
+) -> np.ndarray:
+    """
+    Reweight a noisy tile to better match the real noisy data.
+
+    We expect the addition of a noise tile + signal to have inflated the values in the array,
+    so we'll scale it by a value derived from the maximum values in the noisy tile and the
+    corresponding clean tile.
+
+    :param noisy_tile: a simulated noise tile - clean signal with noise applied. e.g. derived
+                       from add_noise()
+    :param clean_tile: the maximum current value in the corresponding clean tile
+
+    :returns: the reweighted noisy tile
+    :raises ReweightingError: if clean_tile_maximum is NaN or non-positive
+    """
+    if np.isnan(clean_tile_maximum):
+        raise ReweightingError(
+            f"clean_tile_maximum is NaN - cannot reweight noisy tile.\n"
+            "\tMaybe you meant to use np.nanmax()?"
+        )
+    if clean_tile_maximum <= 0:
+        raise ReweightingError(
+            f"clean_tile_maximum isn't positive ({clean_tile_maximum:.3f})."
+            "This shouldn't be possible."
+        )
+
+    # We want to scale such that the new maximum is halfway between the current maximum
+    # and the clean maximum
+    noisy_max = np.nanmax(noisy_tile)
+    scale_factor = (clean_tile_maximum + noisy_max) / (2 * noisy_max)
+
+    return noisy_tile * scale_factor
+
+
+def add_noise(
+    clean_target: np.ndarray,
+    noise_quilt: np.ndarray,
+    strength_map: np.ndarray,
+    rng: np.random.Generator,
+) -> np.ndarray:
+    """
+    Add noise to a target, choosing a random weighting factor for the noise strength
+    and scaling the result to better match real noisy data.
+
+    :param clean_target: the clean target currents
+    :param noise_quilt: the noise quilt to add
+    :param rng: random number generator
+
+    :returns: the noisy target data, with random-strength noise added and weighted to match
+              real noisy data.
+
+    :raises NoiseError: if the shapes of clean_target and noise_quilt do not match
+    """
+    if clean_target.shape != noise_quilt.shape:
+        raise NoiseApplicationError(
+            "Shapes of clean target and noise quilt do not match: "
+            f"{clean_target.shape} vs {noise_quilt.shape}"
+        )
+    if clean_target.shape != strength_map.shape:
+        raise NoiseApplicationError(
+            "Shapes of clean target and strength map do not match: "
+            f"{clean_target.shape} vs {strength_map.shape}"
+        )
+
+    weight = rng.uniform(0.5, 2.5)
+    noisy = _weighted_add(
+        clean_target, noise_quilt, strength_map, weighting_factor=weight
+    )
+
+    return reweight_noisy_tile(noisy, np.nanmax(clean_target))
--- src/tests/ut/test_applying_noise.py
+++ src/tests/ut/test_applying_noise.py
@@ -2,6 +2,7 @@
 Tests for the noise application utils
 """
 
+import pytest
 import numpy as np
 
 from current_denoising.generation import applying_noise
@@ -16,10 +17,184 @@ def test_noise_strength_land_mask():
     map[(map % 3).astype(bool)] = np.nan
 
     assert (
-        np.isnan(map) == np.isnan(applying_noise.noise_strength_map(map, filter_size=5))
+        np.isnan(map) == ((applying_noise.noise_strength_map(map, filter_size=5) == 0))
     ).all()
 
     assert (
-        np.isnan(map)
-        == np.isnan(applying_noise.noise_strength_map(map, filter_size=50))
+        np.isnan(map) == ((applying_noise.noise_strength_map(map, filter_size=50) == 0))
     ).all()
+
+
+def test_weighted_add_no_weights():
+    """
+    Check that we add noise correctly when the weighting factor is 1
+    """
+    clean = np.ones((5, 5))
+    noise = np.arange(25, dtype=np.float32).reshape(5, 5)
+    strength_map = np.ones_like(noise)
+
+    noisy = applying_noise._weighted_add(clean, noise, strength_map, weighting_factor=2)
+
+    np.testing.assert_allclose(noisy, clean + noise * 2)
+
+
+def test_weighted_add():
+    """
+    Check we can add noise correctly with a weighting factor
+    """
+    clean = np.ones((5, 5))
+    noise = np.arange(25, dtype=np.float32).reshape(5, 5)
+    strength_map = np.array(
+        [
+            [1, 1, 2, 2, 2],
+            [1, 1, 2, 2, 2],
+            [1, 1, 2, 2, 2],
+            [1, 1, 2, 2, 2],
+            [1, 1, 2, 2, 2],
+        ]
+    )
+
+    weight = 2
+    expected = noise * strength_map * 2 + clean
+
+    noisy = applying_noise._weighted_add(
+        clean, noise, strength_map, weighting_factor=weight
+    )
+
+    np.testing.assert_allclose(noisy, expected)
+
+
+def test_weighted_add_nan_weight():
+    """
+    Check we get the right error if there are NaNs in the strength map
+    """
+    clean = np.ones((3, 3))
+    noise = np.ones_like(clean)
+    strength = np.ones_like(noise)
+    weight = 2
+    applying_noise._weighted_add(clean, noise, strength, weighting_factor=weight)
+
+    strength[1, 1] = np.nan
+    with pytest.raises(applying_noise.RescalingError):
+        applying_noise._weighted_add(clean, noise, strength, weighting_factor=weight)
+
+
+def test_weighted_add_nans():
+    """
+    Check that we add noise correctly with NaNs in the input
+    """
+    clean = np.ones((5, 5))
+    clean[0, 0] = np.nan
+    clean[1, 1] = np.nan
+    noise = np.arange(25, dtype=np.float32).reshape(5, 5)
+    strength = np.ones_like(noise)
+
+    noisy = applying_noise._weighted_add(clean, noise, strength, weighting_factor=2)
+
+    expected = clean + noise * 2
+    expected[0, 0] = np.nan
+    expected[1, 1] = np.nan
+
+    np.testing.assert_allclose(noisy, expected)
+
+
+def test_reweight_noise():
+    """
+    Check that the reweighting gives us the right values
+    """
+    before = np.arange(9).reshape(3, 3).astype(np.float32)
+    clean_max = 4.0
+
+    np.testing.assert_allclose(
+        0.75 * before, applying_noise.reweight_noisy_tile(before, clean_max)
+    )
+
+
+def test_reweight_nans():
+    """
+    Check the reweighting works when there are NaNs in the input
+    """
+    before = np.arange(9).reshape(3, 3).astype(np.float32)
+    before[1, 1] = np.nan
+    clean_max = 4.0
+
+    np.testing.assert_allclose(
+        0.75 * before, applying_noise.reweight_noisy_tile(before, clean_max)
+    )
+
+
+def test_reweight_nan_max():
+    """
+    Check that we raise an error if the clean tile maximum is NaN
+    """
+    with pytest.raises(applying_noise.NoiseApplicationError):
+        applying_noise.reweight_noisy_tile(np.empty((3, 3)), np.nan)
+
+
+def test_reweight_zero_max():
+    """
+    Check that we raise an error if the clean tile maximum is zero
+    """
+    with pytest.raises(applying_noise.NoiseApplicationError):
+        applying_noise.reweight_noisy_tile(np.empty((3, 3)), 0.0)
+
+
+def test_add_noise_wrong_shape():
+    """
+    Check that we raise an error if the shapes of the clean target and noise quilt or strength map
+    don't match
+    """
+    with pytest.raises(applying_noise.NoiseApplicationError):
+        applying_noise.add_noise(
+            np.empty((5, 5)), np.empty((3, 3)), np.empty((5, 5)), rng=None
+        )
+
+    with pytest.raises(applying_noise.NoiseApplicationError):
+        applying_noise.add_noise(
+            np.empty((5, 5)), np.empty((5, 5)), np.empty((3, 3)), rng=None
+        )
+
+
+class MockRNG:
+    """Fake RNG class for testing"""
+
+    def __init__(self, *, seed: int):
+        """Initialize with a fixed seed value"""
+        self.seed = seed
+
+    def uniform(self, *args) -> int:
+        """Returns the seed to mock a random number"""
+        return self.seed
+
+
+def test_add_noise():
+    """
+    Check that we add noise correctly with a random weighting factor
+    and a simple strength map
+    """
+
+    clean = np.ones((5, 5))
+    noise = np.arange(25, dtype=np.float32).reshape(5, 5)
+    strength_map = np.ones_like(noise)
+    strength_map[:, 2:] += 1
+
+    # Simulate some land
+    clean[0, 0] = np.nan
+
+    # The mock RNG just returns the seed
+    seed = 2
+    expected = (49 / 97) * np.array(
+        [
+            [1.0, 3.0, 9.0, 13.0, 17.0],
+            [11.0, 13.0, 29.0, 33.0, 37.0],
+            [21.0, 23.0, 49.0, 53.0, 57.0],
+            [31.0, 33.0, 69.0, 73.0, 77.0],
+            [41.0, 43.0, 89.0, 93.0, 97.0],
+        ]
+    )
+    expected[0, 0] = np.nan
+
+    noisy = applying_noise.add_noise(clean, noise, strength_map, MockRNG(seed=seed))
+
+    np.testing.assert_allclose(noisy, expected)
+    assert np.isnan(noisy[0, 0])
