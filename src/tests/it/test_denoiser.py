ca4a1975ebeedbc4c8ac768543c0decbc8aca3f5
Richard Lane <richard.lane@bristol.ac.uk>
2025-11-25 17:48:33 +0000

    test for training

--- src/tests/it/test_denoiser.py
+++ src/tests/it/test_denoiser.py
@@ -2,7 +2,9 @@
 Test for the denoising model + utility functions
 """
 
-from current_denoising.denoising import model
+import numpy as np
+
+from current_denoising.denoising import model, data, train
 
 
 def test_channels():
@@ -10,3 +12,25 @@ def test_channels():
     Check we get the right number of channels
     """
     assert model._channels(4, 8) == [8, 16, 32, 64]
+
+
+def test_train():
+    """
+    Check we can train a simple model
+    """
+    tile = np.arange(12).reshape((3, 2, 2))
+    train_loader = data.dataloader(
+        tile, tile, data.DataConfig(train=True, batch_size=2, num_workers=0)
+    )
+    val_loader = data.dataloader(
+        tile, tile, data.DataConfig(train=False, batch_size=2, num_workers=0)
+    )
+
+    n_epochs = 1
+    net = model.get_attention_unet(2, 0)
+    net, train_loss, val_loss = train.train_model(
+        net, n_epochs=1, train_data=train_loader, val_data=val_loader
+    )
+
+    assert train_loss.shape == (1, 1)  # We threw away the second incomplete batch
+    assert val_loss.shape == (1, 2)
